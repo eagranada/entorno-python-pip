c3b38a644eae4d7156610cfaaf43a55454b3aa62
eagranada <eagranada22@gmailo.com>
2023-10-18 16:59:05 -0500

    carpeta de juego piedra-papel-tijera

---
+++ game/main.py
@@ -0,0 +1,83 @@
+'''
+print("Hola, esto es python.")
+print("Hola, soy Edwin y tengo 37 años.")
+
+print(12 + 5)
+print(12 - 5)
+print(10 * 5)
+print(20 / 5)
+'''
+#esto es un comentario de una sola linea
+"""
+Este es 
+un comentario
+de varias lineas
+"""
+import random
+options = ('piedra', 'papel', 'tijera')
+
+computer_wins = 0
+user_wins = 0
+
+rounds = 1
+
+while True:
+
+    print('*' * 10)
+    print('ROUND', rounds)
+    print('*' * 10)
+
+    print('computer_wins', computer_wins)
+    print('user_wins', user_wins)
+
+    user_option = input('piedra, papel o tijera => ')
+    user_option = user_option.lower()
+
+    rounds += 1
+
+    if not user_option in options:
+      print('esa opcion no es valida')
+      continue
+
+    computer_option = random.choice(options)
+
+    print('User option =>', user_option)
+    print('Computer option =>', computer_option)
+
+    if user_option == computer_option:
+        print('Empate!')
+    elif user_option == 'piedra':
+        if computer_option == 'tijera':
+            print('piedra gana a tijera')
+            print('user gano!')
+            user_wins += 1
+        else:
+            print('Papel gana a piedra')
+            print('computer gano!')
+            computer_wins += 1
+    elif user_option == 'papel':
+        if computer_option == 'piedra':
+            print('papel gana a piedra')
+            print('user gano')
+            user_wins += 1
+        else:
+            print('tijera gana a papel')
+            print('computer gano!')
+            computer_wins += 1
+    elif user_option == 'tijera':
+        if computer_option == 'papel':
+            print('tijera gana a papel')
+            print('user gano!')
+            user_wins += 1
+        else:
+            print('piedra gana a tijera')
+            print('computer gano!')
+            computer_wins += 1
+
+    if computer_wins == 2:
+      print('El ganador es la computadora')
+      break
+
+    if user_wins == 2:
+      print('El ganador es el usuario')
+      break
